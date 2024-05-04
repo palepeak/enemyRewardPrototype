0b3d203004bf77df080eb92503e58760fe75f095
Bobby Wang <bobby.wang25@gmail.com>
2024-05-04 13:58:28 -0700

    add ray trace for color launching max and min distance and collision

--- ColorLauncher/ColorLauncher.gd
+++ ColorLauncher/ColorLauncher.gd
@@ -4,8 +4,11 @@ class_name ColorLauncher extends Node2D
 @export var bomb_residual: PackedScene
 @export var worldColorStore: WorldColorStore
 @export var reload_time = 0.5
+@export var max_range = 300
+@export var minimum_range = 0
 
 @onready var reload_timer: Timer = $Timer
+@onready var raycast: RayCast2D = $RayCast2D
 var can_launch = true
 
 
@@ -24,10 +27,22 @@ func _process(delta):
 	bar.value = value * 100	
 	
 	var local_mouse_position = to_local(get_global_mouse_position())
-	var point_in_x = (local_mouse_position - $Path2D.curve.get_point_position(0)).x * -400/1152
-	var point_in_y = min(-200, (local_mouse_position - $Path2D.curve.get_point_position(0)).y)
+	raycast.target_position = local_mouse_position
+	raycast.force_raycast_update()
+	
+	var target_launch_position = local_mouse_position
+	if (raycast.is_colliding()):
+		target_launch_position = to_local(raycast.get_collision_point())
+	
+	if position.distance_to(target_launch_position) > max_range:
+		target_launch_position = target_launch_position.normalized() * max_range
+	elif position.distance_to(target_launch_position) < minimum_range:
+		target_launch_position = target_launch_position.normalized() * minimum_range
+	print(target_launch_position.length())
+	var point_in_x = (target_launch_position - $Path2D.curve.get_point_position(0)).x * -400/1152
+	var point_in_y = min(-200, (target_launch_position - $Path2D.curve.get_point_position(0)).y)
 	# $Sprite2D2.global_position = $Sprite2D2.get_global_mouse_position()
-	$Path2D.curve.set_point_position(1, local_mouse_position)
+	$Path2D.curve.set_point_position(1, target_launch_position)
 	$Path2D.curve.set_point_in(1, Vector2(point_in_x, point_in_y))
 	$Path2D.curve.set_point_out(0, Vector2(-point_in_x, point_in_y))
 	$Line2D.points = $Path2D.curve.get_baked_points()
--- Levels/GPUParticles2D.gd
+++ Levels/GPUParticles2D.gd
@@ -3,7 +3,12 @@ extends GPUParticles2D
 
 func start_darkness(level_map: TileMap):
 	var level_map_bounding_cells = level_map.get_used_rect()
-	var level_size = 32 * (level_map_bounding_cells.size) / 2
+	var level_size = 32 * (level_map_bounding_cells.size)
+	
+	var screen_size = Vector2i(get_viewport_rect().size)
+	var emission_box_extents = level_size/2 + screen_size
 	var shader = process_material as ShaderMaterial
-	shader.set_shader_parameter("emission_box_extents", [level_size.x, level_size.y, 1])
-	amount = 8000
+	shader.set_shader_parameter("emission_box_extents", [emission_box_extents.x, emission_box_extents.y, 1])
+	shader.set_shader_parameter("screen_size", screen_size)
+	shader.set_shader_parameter("level_size", level_size)
+	amount = 12000
