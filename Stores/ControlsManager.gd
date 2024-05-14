281755fff1603ea2903e299b3d5f89ac8ca6f268
Bobby Wang <bobby.wang25@gmail.com>
2024-05-14 14:13:08 -0700

    updated controller aiming

--- Stores/ControlsManager.gd
+++ Stores/ControlsManager.gd
@@ -2,10 +2,18 @@ extends Node
 
 var using_mouse = true
 var _last_mouse_movement = null
+
+var _aim_target_local = Vector2(0, 0)
+var _aim_target_center_mode_local = Vector2(0, 0)
+var _current_aim_target_local = Vector2(0, 0)
+var _controller_aim_center_mode = false
+var _controller_aim_sensitivity = 8.0
+
+
 func _ready():
 	Input.joy_connection_changed.connect(_on_joy_connection_changed)
 
-func _process(_delta):
+func _process(delta):
 	var joy_stick_aim = Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down")
 	if using_mouse && joy_stick_aim != Vector2.ZERO:
 		using_mouse = false
@@ -15,6 +23,19 @@ func _process(_delta):
 		print("using mouse")
 		_last_mouse_movement = Input.get_last_mouse_velocity()
 		using_mouse = true
+	
+	if Input.is_action_just_pressed("switch_controller_aim_mode"):
+		_controller_aim_center_mode = !_controller_aim_center_mode
+	
+	if _controller_aim_center_mode:
+		_aim_target_center_mode_local = Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down")
+		_current_aim_target_local = _aim_target_center_mode_local
+	else:
+		var aim_target = Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down")
+		_aim_target_local = (aim_target*delta*_controller_aim_sensitivity + _aim_target_local)
+		if _aim_target_local.length() > 1:
+			_aim_target_local = _aim_target_local.normalized()
+		_current_aim_target_local = _aim_target_local
 
 
 func get_aim_target_local(player: Player, max_range: int = 1) -> Vector2:
@@ -25,8 +46,7 @@ func get_aim_target_global(player: Player, max_range: int = 1) -> Vector2:
 	if using_mouse:
 		return player.get_global_mouse_position()
 	else:
-		var joy_stick_aim = Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down")
-		return player.to_global(joy_stick_aim * max_range)
+		return player.to_global(_current_aim_target_local * max_range)
 
 func get_aim_target_viewport(parent: Node2D) -> Vector2:
 	if using_mouse:
@@ -38,8 +58,7 @@ func get_aim_target_viewport(parent: Node2D) -> Vector2:
 		)
 		return (bound_position - viewport/2)/3
 	else:
-		var joy_stick_aim = Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down")
-		return joy_stick_aim * parent.get_viewport_rect().size/6
+		return _current_aim_target_local * parent.get_viewport_rect().size/6
 
 
 func _on_joy_connection_changed(device_id, connected):
